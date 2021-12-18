1d2d90951da0e699b1d9231b1339d30e60ba4c27
ILLO YOON <yildbs@gmail.com>
2021-12-18 16:17:38 +0900

    working

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 import login_info
 import time
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+import requests
 
 driver = webdriver.Chrome('/Users/illo.yoon/Workspace/Python/chromedriver')
 driver.implicitly_wait(3)
@@ -29,6 +31,58 @@ input_search_keyword.send_keys('hand without ring')
 input_search_keyword.send_keys(Keys.ENTER)
 
 driver.implicitly_wait(5)
+
+# /html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/a/div/div[1]/div/div/div/div/div/img
+# images = driver.find_elements_by_xpath('/html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/a/div/div[1]/div/div/div/div/div/img')
+
+# LOOP
+import os
+
+imagedir = 'images'
+try:
+    os.makedirs(imagedir, exist_ok=True)
+except :
+    pass
+
+target_num_images = 10000
+links_saved = set()
+while True:
+    try:
+        # get images
+        images = driver.find_elements_by_xpath("//img[@srcset]")
+
+        for image in images:
+            try:
+                links = image.get_property('srcset')
+                url = list(filter(lambda x: x.startswith('https'), links.split(' ')))[-1]
+                if url not in links_saved:
+                    links_saved.add(url)
+
+                    # download image
+                    imagename = url[url.rfind('/')+1:]
+                    response = requests.get(url)
+                    file = open(imagedir + '/'+imagename, "wb")
+                    file.write(response.content)
+                    file.close()
+            except Exception as e:
+                pass
+
+        if len(links_saved) > target_num_images:
+            break
+
+        if len(images)>0:
+            action = ActionChains(driver)
+            action.move_to_element(images[-1]).perform()
+        else:
+            break
+        pass
+    except Exception as e:
+        pass
+
+# /html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div[1]/a/div/div[1]/div/div/div/div/div/img
+# /html/body/div[1]/div[1]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div/div[1]/div[2]/div/div/div/div/div[1]/div[1]/a/div/div[1]/div/div/div/div/div/img
+# id가 something 인 element 를 찾음
+# some_tag = driver.find_element_by_id('something')
 
 
 print('hello')

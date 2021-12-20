1eadd56a3248bb742fcdb3f19effd7487200d1a7
ILLO YOON <yildbs@gmail.com>
2021-12-20 17:05:57 +0900

    can search multiple words

--- conf.py
+++ conf.py
@@ -1,3 +1,20 @@
-search_word = 'hand'
-target_num_images = 10000
+search_words = ['tartan fabric'
+            , 'silk fabric'
+            , 'Plaid fabric'
+            , 'Plaid fabric pattern'
+            , 'tartan fabric pattern'
+            , 'Fabric texture material'
+            , 'windowpane Fabric texture material'
+            , 'cloth texture material'
+            , 'knit texture material'
+            , 'Knit fabric texture'
+            , 'checkerboard fabric texture'
+            , 'fabric pattern texture'
+            , 'Wool fabric texture'
+        ]
+
+
+# target_num_images = 10000
+target_num_images = 1000
 chromedriver_path = '/Users/illo.yoon/Workspace/Python/chromedriver'
+
--- main.py
+++ main.py
@@ -35,52 +35,82 @@ button_login.click()
 time.sleep(20)
 driver.implicitly_wait(10)
 
-# 검색어 입력하기
-input_search_keyword = driver.find_element_by_css_selector('#searchBoxContainer > div > div > div.ujU.zI7.iyn.Hsu > input[type=text]')
-input_search_keyword.send_keys(conf.search_word)
-input_search_keyword.send_keys(Keys.ENTER)
-
-driver.implicitly_wait(5)
-
 imagedir = 'images'
 try:
     os.makedirs(imagedir, exist_ok=True)
-except :
+except:
     pass
 
-target_num_images = conf.target_num_images
-links_saved = set()
+for search_word in conf.search_words:
+    # 검색어 입력하기
+    input_search_keyword = driver.find_element_by_css_selector('#searchBoxContainer > div > div > div.ujU.zI7.iyn.Hsu > input[type=text]')
+    for _ in range(100):
+        input_search_keyword.send_keys(Keys.BACKSPACE)
+        time.sleep(0.01)
+    input_search_keyword.send_keys(search_word)
+    input_search_keyword.send_keys(Keys.ENTER)
 
-# LOOP
-while True:
+    driver.implicitly_wait(5)
+
+    print('Searching word (' + search_word + ')')
+
     try:
-        # get images
-        images = driver.find_elements_by_xpath("//img[@srcset]")
-
-        for image in images:
-            try:
-                links = image.get_property('srcset')
-                url = list(filter(lambda x: x.startswith('https'), links.split(' ')))[-1]
-                if url not in links_saved:
-                    links_saved.add(url)
-
-                    # download image
-                    imagename = url[url.rfind('/')+1:]
-                    response = requests.get(url)
-                    file = open(imagedir + '/'+imagename, "wb")
-                    file.write(response.content)
-                    file.close()
-            except Exception as e:
-                pass
-
-        if len(links_saved) > target_num_images:
-            break
-
-        if len(images)>0:
-            action = ActionChains(driver)
-            action.move_to_element(images[-1]).perform()
-        else:
-            break
+        os.makedirs(imagedir + '/' + search_word, exist_ok=True)
+    except:
         pass
-    except Exception as e:
-        pass
+
+    links_saved = set()
+
+    target_num_images = conf.target_num_images
+    force_scroll_down = 0
+    # LOOP
+    while True:
+
+        try:
+            # get images
+            images = driver.find_elements_by_xpath("//img[@srcset]")
+
+            count_new_images = 0
+
+            for image in images:
+                try:
+                    links = image.get_property('srcset')
+                    url = list(filter(lambda x: x.startswith('https'), links.split(' ')))[-1]
+                    if url not in links_saved:
+                        links_saved.add(url)
+
+                        count_new_images += 1
+
+                        # download image
+                        imagename = url[url.rfind('/')+1:]
+                        response = requests.get(url)
+                        file = open(imagedir + '/' + search_word + '/' + imagename, "wb")
+                        file.write(response.content)
+                        file.close()
+                except Exception as e:
+                    pass
+
+            print(str(len(links_saved)) + ' / ' + str(target_num_images))
+
+            if len(links_saved) > target_num_images:
+                break
+
+            if count_new_images == 0:
+                if force_scroll_down < 5:
+                    # driver.execute_script("window.scrollTo(0, 300)")
+                    driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
+                    force_scroll_down += 1
+                    time.sleep(4)
+                    driver.implicitly_wait(4)
+                else:
+                    print('Force break')
+                    break
+            elif len(images)>0:
+                action = ActionChains(driver)
+                action.move_to_element(images[-1]).perform()
+                force_scroll_down = 0
+            else:
+                break
+            pass
+        except Exception as e:
+            pass
